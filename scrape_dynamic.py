b4e818023ae75cf85dbd029d2db52c8d61d2fcea
Tyson <tysonmd@apollo-is.com>
2022-07-13 16:01:56 -0500

    teckstacks.io and darkreading scraped by api call

--- scrape_dynamic.py
+++ scrape_dynamic.py
@@ -5,24 +5,27 @@ from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.options import Options
 import time
 
-
-def get_dynamic(url):
+def start_driver():
+    global DRIVER
     chrome_options = Options()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--remote-debugging-port=9222')
     chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-
-
+    
     '''PATH TO CHROME DRIVER NEEDS TO BE UPDATED to "/usr/bin/chromedriver"'''
-    path = "C:\Program Files (x86)\chromedriver.exe"
-    driver = webdriver.Chrome(path, options=chrome_options)
-    driver.get(url)
+    path = "/usr/bin/chromedriver"
+    DRIVER = webdriver.Chrome(path, options=chrome_options)   
+
+def get_dynamic(url):
+    DRIVER.get(url)
     time.sleep(3)
-    soup = BeautifulSoup(driver.page_source, "html.parser")
-    driver.quit()
+    soup = BeautifulSoup(DRIVER.page_source, "html.parser")
     return soup
+
+def quit_driver():
+    DRIVER.quit()
 
 
 #get_dynamic('https://www.bleepingcomputer.com/search/?cx=partner-pub-0920899300397823%3A3529943228&cof=FORID%3A10&ie=UTF-8&q=walmart')
--- technology.py
+++ technology.py
@@ -1,47 +1,63 @@
-from bs4 import BeautifulSoup
-import scrape_dynamic
 
-'''
-fetch company technology stack information from techstacks.io
-'''
-def get_content(l_tech):
+import requests
+import json
+import re
+# '''
+# fetch company technology stack information from techstacks.io
+# '''
+def articles(search_name):
     r_dict = {}
-    for tech in l_tech:
-        # print('in loop')
-        title = tech.find("h2").get_text()
-        r_dict[title] = [t.find("img").get("src") for t in tech.find_all("div", class_="flex", style="max-width: 300px;")]
-    
-    return r_dict
-
-def get_articles(soup):
-    r_dict = {}
-    articles_class = soup.find("div", class_="details-html", style="padding: 1em;")
-    articles = articles_class.find_all("li")
-    for article in articles:
-        r_dict[article.find('a').get_text()] = article.find("a").get("href")
-    return r_dict
-    
-
-def get_tech(company):
-    r_list = []
-    search_url = "https://techstacks.io/stacks/?nameContains=" + company
-    b_url = "https://techstacks.io"
-    search_soup = scrape_dynamic.get_dynamic(search_url)
-    # print(soup)
-    # print(url)
-    try:
-        soup = scrape_dynamic.get_dynamic(b_url + search_soup.find("div", class_="container fluid grid-list-md").find_all("div", class_="flex xs3")[0].find("a").get("href"))
-        technologies_class = soup.find("div", class_="container body grid-list-md")
-        # print(technologies_class)
-        tech = technologies_class.find_all("div", class_="layout tech-info")
-        # print(tech)
-        r_list.append(get_content(tech))
-        r_list.append(get_articles(soup))
-    except Exception as e:
-        print(e)
+    #now use company name to query for the tech stack
+    tech_url = "https://techstacks.io/json/reply/GetTechnologyStack?slug=" + search_name
+    response = requests.get(tech_url)
+    if response.ok:
+        pattern = re.compile(r"\- \[(.*?)\]\((.*?)\)")
+        jdata = json.loads(response.content)
+        list_lines = jdata['result']['details'].split("\n")
+        for line in list_lines:
+            # print(line)
+            r = pattern.search(line)
+            if r != None:
+                r_dict[r.group(1)] = r.group(2)
+    else:
         return {}
 
-    return r_list
+    return r_dict
 
-# l = get_tech("AT&T")
-# print(l)
+
+def tech_stack(search_name):
+    r_dict = {}
+    #now use company name to query for the tech stack
+    tech_url = "https://techstacks.io/json/reply/GetTechnologyStack?slug=" + search_name
+    response = requests.get(tech_url)
+    if response.ok:
+        jdata = json.loads(response.content)
+        l_tech = jdata['result']['technologyChoices']
+        for tech in l_tech:
+            if tech['tier'] not in r_dict:
+                r_dict[tech['tier']] = [tech['logoUrl']]
+            else:
+                r_dict[tech['tier']].append(tech['logoUrl'])
+    else:
+        return {}
+    
+    return r_dict
+
+def get_tech(company):
+    name_url = "https://techstacks.io/json/reply/QueryTechStacks?include=total&nameContains=" + company
+    response = requests.get(name_url)
+    if not response.ok:
+        return []
+
+    try:
+        jdata = json.loads(response.content)
+        first_result = jdata['results'][0]
+        search_name = first_result['slug']
+
+        r_list = []
+        r_list.append(tech_stack(search_name))
+        r_list.append(articles(search_name))
+    except Exception:
+        return []
+
+    return r_list
